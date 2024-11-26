e9e8b0d029d82aada7b5caf18ed1d7fa9a29cafc
unknown <administrador@CC.CORELIA>
2024-11-26 12:02:07 -0500

    Inicializando el frontend separado

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    server: {
+        host: '0.0.0.0', // Permite que la aplicación esté accesible externamente
+        port: process.env.PORT || 3000, // Usa el puerto proporcionado por Render o 3000 como fallback
+    },
+});
